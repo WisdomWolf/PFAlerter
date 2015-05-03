baf09011971f0d35bf09edebb528bfaf8c690451
WisdomWolf <WisdomWolf@gmail.com>
2015-05-03 15:18:19 -0400

    adding for merge

---
+++ encodePassword.py
@@ -0,0 +1,15 @@
+#! python3
+import base64
+import codecs
+import clipboard
+
+with codecs.open('password.txt', 'w', 'utf-8') as simpleOutput:
+    x = input('Enter password to encode:\n')
+    b = str.encode(x)
+    print('password encoded:')
+    y = (base64.b64encode(b)).decode()
+    simpleOutput.write(y)
+    print(y)
+    clipboard.copy(y)
+    input('complete')
+
