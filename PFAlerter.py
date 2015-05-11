1e033c10ff8d587ce8a96b9cf7974e996cdaea7e
WisdomWolf <WisdomWolf@gmail.com>
2015-05-11 16:01:02 -0400

    migrated more methods to new class for encapsulation

--- PFAlerter.py
+++ PFAlerter.py
@@ -68,59 +68,46 @@ class PFAlert:
             
         server.quit()
     
-                
 
-def importConfig():
-    config = ConfigParser()
-    config.read('config.ini')
-    global theurl, username, password, json_req, headers, smtpServer, serverPort, user, pwd, emailRecipients, FROM
-    theurl = config['PF Listener']['url']
-    username = config['PF Listener']['username']
-    password = config['PF Listener']['password']
-    json_req = 'application/json'
-    headers = { 'Accept' : json_req }
-    smtpServer = config['Email']['smtpServer']
-    try:
-        serverPort = int(config['Email']['serverPort'])
-    except ValueError:
-        input('malformed config file. <Bad Server Port> Aborting...')
-        os._exit(0)
-    user = config['Email']['senderAddress']
-    pwd = (base64.b64decode(config['Email']['senderPassword'])).decode() #basic password encoding
-    emailRecipients = config['Email']['receiverAddresses']
-    FROM = config['Email']['from']
+    """builds the authentication and request handlers"""
+    def buildRequester(self):
+        passman = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+        # this creates a password manager
+        passman.add_password(None, self.theurl, self.serverUsername, self.serverPassword)
+        # because we have put None at the start it will always
+        # use this username/password combination for  urls
+        # for which `theurl` is a super-url
 
-"""builds the authentication and request handlers"""
-def buildRequester():
-    passman = urllib.request.HTTPPasswordMgrWithDefaultRealm()
-    # this creates a password manager
-    passman.add_password(None, theurl, username, password)
-    # because we have put None at the start it will always
-    # use this username/password combination for  urls
-    # for which `theurl` is a super-url
+        authhandler = urllib.request.HTTPBasicAuthHandler(passman)
+        # create the AuthHandler
 
-    authhandler = urllib.request.HTTPBasicAuthHandler(passman)
-    # create the AuthHandler
+        opener = urllib.request.build_opener(authhandler)
 
-    opener = urllib.request.build_opener(authhandler)
+        urllib.request.install_opener(opener)
+        # All calls to urllib.request will now use our handler
+        # Make sure not to include the protocol in with the URL, or
+        # HTTPPasswordMgrWithDefaultRealm will be very confused.
+        # You must (of course) use it when fetching the page though.
+        # authentication is now handled automatically for us
+        return
 
-    urllib.request.install_opener(opener)
-    # All calls to urllib.request will now use our handler
-    # Make sure not to include the protocol in with the URL, or
-    # HTTPPasswordMgrWithDefaultRealm will be very confused.
-    # You must (of course) use it when fetching the page though.
-    # authentication is now handled automatically for us
-    return
-
-"""pulls the JSON data and puts it into Python object format"""
-def pullJSON():
-    req = urllib.request.Request(theurl, None, headers)
-    response = urllib.request.urlopen(req)
-    str_response = response.readall().decode('utf-8')
-    data = json.loads(str_response)
-    
-    return data
-    
+    """pulls the JSON data and puts it into Python object format"""
+    def pullJSON(self):
+        req = urllib.request.Request(self.theurl, None, HEADERS)
+        response = urllib.request.urlopen(req)
+        str_response = response.readall().decode('utf-8')
+        
+        return json.loads(str_response)
+        
+    def JSONTest(self):    
+        self.buildRequester()
+        jsonData = self.pullJSON()
+        jsonToTextFile(jsonData)
+        listenerStr = "Listeners: \r\n----------\r\n"
+        listenerList = jsonData['ListenersContainer']['Listener']
+        listenerStr += pullJSONValue('name', listenerList)
+        print(listenerStr)
+        
 def pullJSONFromTextFile(fileIn):
     data = open(fileIn).read()
         
@@ -149,7 +136,6 @@ def buildTestJSON(fileIn, fileOut):
     jsonToTextFile(data, fileOut)
     return
     
-
 """pulls a specific element from the JSON list"""
 def pullJSONValue(key, list):
     keyStr = ""
@@ -159,15 +145,6 @@ def pullJSONValue(key, list):
         else:
             keyStr += str(j) + ". " + str(i[key]) + "\r\n"
     return keyStr
-
-def JSONTest():    
-    buildRequester()
-    jsonData = pullJSON()
-    jsonToTextFile(jsonData)
-    listenerStr = "Listeners: \r\n----------\r\n"
-    listenerList = jsonData['ListenersContainer']['Listener']
-    listenerStr += pullJSONValue('name', listenerList)
-    print(listenerStr)
     
 #sendEmail()
 #with codecs.open('listener_list.txt', 'w+', 'utf-8') as save_file:
